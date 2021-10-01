8dd42b5d28a70a203fd5b549e19b374cdf04f4df
xrv0 <test@test.de>
2021-10-01 18:21:45 +0200

    Unit test for `extractAmountFromUnitString`

---
+++ src/__tests__/utils/parse-utils.spec.ts
@@ -0,0 +1,23 @@
+import { extractAmountFromUnitString } from '../../swap/utils/parse-utils';
+
+test('should parse btc amount string correctly', () => {
+  expect(extractAmountFromUnitString('0.1 BTC')).toBe(0.1);
+  expect(extractAmountFromUnitString('0.0045 BTC')).toBe(0.0045);
+});
+
+test('should parse xmr amount string correctly', () => {
+  expect(extractAmountFromUnitString('0.1 XMR')).toBe(0.1);
+  expect(extractAmountFromUnitString('0.0045 XMR')).toBe(0.0045);
+});
+
+test('should fail with invalid strings', () => {
+  expect(() => extractAmountFromUnitString('0.1')).toThrow();
+  expect(() => extractAmountFromUnitString('BTC')).toThrow();
+  expect(() => extractAmountFromUnitString('')).toThrow();
+  expect(() =>
+    extractAmountFromUnitString(null as unknown as string)
+  ).toThrow();
+  expect(() =>
+    extractAmountFromUnitString(undefined as unknown as string)
+  ).toThrow();
+});
