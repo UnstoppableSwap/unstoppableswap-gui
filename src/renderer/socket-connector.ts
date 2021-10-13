fc617565ad450aa9837fa16834556ff4f5964733
Binarybaron <new@email>
2021-10-14 00:43:55 +0200

    Enable CORS and switch to subdomain api

--- src/renderer/socket-connector.ts
+++ src/renderer/socket-connector.ts
@@ -3,7 +3,7 @@ import { store } from '../store/store';
 import { setProviders } from '../store/features/swap/providersSlice';
 
 export default async () => {
-  const socket = io('https://unstoppableswap.net', {
+  const socket = io('https://api.unstoppableswap.net', {
     path: '/api/socket.io',
   });
   socket.on('provider-refresh', (providerList) => {
