1273712f3821e4988d795c4721fcd0c11b92afb5
binarybaron <86064887+binarybaron@users.noreply.github.com>
2022-11-04 23:05:17 +0100

    Change Alerts to filled style

--- src/renderer/components/alert/ConfidentialityAlert.tsx
+++ src/renderer/components/alert/ConfidentialityAlert.tsx
@@ -2,7 +2,7 @@ import { Alert } from '@material-ui/lab';
 
 export default function ConfidentialityAlert() {
   return (
-    <Alert severity="warning">
+    <Alert severity="warning" variant="filled">
       This page contains confidential information including private keys. Keep
       this information to yourself. You will lose your funds if you do not!
     </Alert>
