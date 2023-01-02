583aa5fbde6cf1543ea27a968835cefa6dc7cd25
binarybaron <86064887+binarybaron@users.noreply.github.com>
2023-01-02 19:17:01 +0100

    Add AutoUpdater

---
+++ src/main/updater.ts
@@ -0,0 +1,25 @@
+import { autoUpdater } from 'electron-updater';
+import logger from '../utils/logger';
+
+export default async function initAutoUpdater() {
+  autoUpdater.on('update-downloaded', (info: any) => {
+    logger.info({ info }, 'Update downloaded');
+  });
+  autoUpdater.on('update-available', (info: any) => {
+    logger.info({ info }, 'Update available');
+  });
+  autoUpdater.on('update-not-available', (info: any) => {
+    logger.info({ info }, 'Update not available');
+  });
+  autoUpdater.on('error', (err: any) => {
+    logger.error({ err }, 'Update error');
+  });
+  autoUpdater.on('checking-for-update', () => {
+    logger.info('Checking for update');
+  });
+
+  autoUpdater.allowPrerelease = true;
+  logger.info('Starting auto updater');
+  console.log(autoUpdater.getFeedURL());
+  await autoUpdater.checkForUpdatesAndNotify();
+}
