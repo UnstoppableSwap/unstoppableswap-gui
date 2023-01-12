b81649e25fcc09d080b6e4f1870adbc1ac7b0193
binarybaron <86064887+binarybaron@users.noreply.github.com>
2023-01-12 13:18:40 +0100

    Add TorStatusBadge to SwapDialog

---
+++ src/renderer/components/modal/swap/pages/TorStatusBadge.tsx
@@ -0,0 +1,17 @@
+import { useAppSelector } from '../../../../../store/hooks';
+import TorIcon from '../../../icons/TorIcon';
+import { IconButton, Tooltip } from '@material-ui/core';
+
+export default function TorStatusBadge() {
+  const tor = useAppSelector((s) => s.tor);
+
+  if(tor.processRunning) {
+    return <Tooltip title="Tor is running in the background">
+      <IconButton>
+        <TorIcon htmlColor="#7D4698" />
+      </IconButton>
+    </Tooltip>;
+  }
+
+  return <></>;
+}
