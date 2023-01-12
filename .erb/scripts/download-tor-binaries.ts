d0ab2800809c7e32ee207dd1c13514935a9e3b4f
binarybaron <86064887+binarybaron@users.noreply.github.com>
2023-01-12 13:17:26 +0100

    Ignore failed chmod, chmod files before packaging

--- .erb/scripts/download-tor-binaries.ts
+++ .erb/scripts/download-tor-binaries.ts
@@ -1,10 +1,28 @@
-import { join } from 'path';
-import { emptyDir } from 'fs-extra';
+import path, { join } from 'path';
+import { chmod, emptyDir, stat } from 'fs-extra';
 import { spawn } from 'child_process';
+import { Binary } from '../../src/models/downloaderModel';
+import { constants } from 'fs';
 
 const TOR_BINARIES_GIT_URL =
   'https://github.com/UnstoppableSwap/static-tor-binaries.git';
 const torBuildDir = join(__dirname, '../../build/bin/tor');
+
+async function makeFileExecutable(binary: Binary) {
+  const fullPath = path.join(binary.dirPath, binary.fileName);
+  const { mode } = await stat(fullPath);
+  await chmod(
+    fullPath,
+    // eslint-disable-next-line no-bitwise
+    mode | constants.S_IXUSR | constants.S_IXGRP | constants.S_IXOTH
+  );
+}
+
+const extractedTorBinaryPaths = [
+  [join(torBuildDir, 'linux'), 'tor'],
+  [join(torBuildDir, 'mac'), 'tor'],
+  [join(torBuildDir, 'win'), 'tor.exe'],
+];
 
 // Delete tor binaries, use Node fs API instead of spawn
 // to avoid spawning a process that we need to kill.
@@ -28,10 +46,18 @@ emptyDir(torBuildDir, (error) => {
     console.log(`Tor Downloader Error: ${e}`);
   });
 
-  ls.on('close', (code: number) => {
+  ls.on('close', async (code: number) => {
     console.log(`Tor Downloader process exited with code ${code}`);
     if (code === 0) {
       console.log('Tor binaries downloaded successfully!');
+      await Promise.all(
+        extractedTorBinaryPaths.map(async (binary) => {
+          await makeFileExecutable({
+            dirPath: binary[0],
+            fileName: binary[1],
+          });
+        })
+      );
     } else {
       console.log('Tor binaries download failed!');
       process.exit(1);
