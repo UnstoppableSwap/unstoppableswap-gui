4bdc9a3e9f554e59cec8dffcac8a7de9a453e8ca
binarybaron <86064887+binarybaron@users.noreply.github.com>
2023-06-11 21:40:09 +0200

    Lint and add key to react list

--- src/renderer/components/pages/help/ElectrumInfoBox.tsx
+++ src/renderer/components/pages/help/ElectrumInfoBox.tsx
@@ -14,19 +14,25 @@ export default function ElectrumInfoBox() {
       title={'Bitcoin Electrum Server'}
       mainContent={
         <Typography variant="subtitle2">
-          The GUI connects to a Bitcoin Electrum Server to retrieve Blockchain data to display some additional information about the state
-          of your swaps
+          The GUI connects to a Bitcoin Electrum Server to retrieve Blockchain
+          data to display some additional information about the state of your
+          swaps
         </Typography>
       }
-      additionalContent={electrumServerConnection ? (
-        <Typography variant="subtitle2">
-          Connected to {electrumServerConnection[1]} on port {electrumServerConnection[2]} over {electrumServerConnection[3]}. We are currently watching {numberOfTrackedTransactions} transactions.
-        </Typography>
-      ) : (
-        <Typography variant="subtitle2">
-          Not connected to any Electrum Server. We are trying to connect to one...
-        </Typography>
-      )}
+      additionalContent={
+        electrumServerConnection ? (
+          <Typography variant="subtitle2">
+            Connected to {electrumServerConnection[1]} on port{' '}
+            {electrumServerConnection[2]} over {electrumServerConnection[3]}. We
+            are currently watching {numberOfTrackedTransactions} transactions.
+          </Typography>
+        ) : (
+          <Typography variant="subtitle2">
+            Not connected to any Electrum Server. We are trying to connect to
+            one...
+          </Typography>
+        )
+      }
       icon={<BitcoinIcon />}
       loading={electrumServerConnection === null}
     />
