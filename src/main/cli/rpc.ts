defc1c45fc2e14c1ef89322cfe434602c1045cb6
binarybaron <86064887+binarybaron@users.noreply.github.com>
2024-05-23 13:28:32 +0200

    Use wildcard to check for log reference id equality

--- src/main/cli/rpc.ts
+++ src/main/cli/rpc.ts
@@ -79,7 +79,7 @@ export async function makeRpcRequest<T>(
         RPC_LOG_EVENT_EMITTER.on((logs) => {
           const relevantLogs = logs.filter(
             (log) =>
-              getCliLogSpanLogReferenceId(log) === logReferenceId ||
+              getCliLogSpanLogReferenceId(log)?.includes(logReferenceId) ||
               (includeGloballyRelevantLogs &&
                 hasCliLogOneOfMultipleSpans(log, GLOBALLY_RELEVANT_SPANS))
           );
@@ -136,8 +136,8 @@ export async function makeBatchRpcRequest<T>(
       });
 
       RPC_LOG_EVENT_EMITTER.on((logs) => {
-        const relevantLogs = logs.filter(
-          (log) => getCliLogSpanLogReferenceId(log) === logReferenceId
+        const relevantLogs = logs.filter((log) =>
+          getCliLogSpanLogReferenceId(log)?.includes(logReferenceId)
         );
         if (relevantLogs.length > 0) {
           logCallback(relevantLogs);
